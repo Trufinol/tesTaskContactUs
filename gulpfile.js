8a3916d4000ed32251854f252336320ea51e0dc2
Nick Verteba <n.verteba@gmail.com>
2016-07-02 10:49:46 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass');
+
+gulp.task('sass', function(){
+    return gulp.src('app/sass/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./app/css'))
+});
+
+gulp.task('watch', function () {
+    gulp.watch('app/sass/*.scss', ['sass']);
+
+});
+
+gulp.task('default', ['watch']);
